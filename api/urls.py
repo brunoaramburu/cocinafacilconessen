d81f5a18a994381788058f4e23370dda227a615a
brunoaramburu <brunoaranburu3@gmail.com>
2022-11-17 18:28:28 -0300

    db

--- api/urls.py
+++ api/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from api.views import BazarPremiumViewSet, CombosViewSet, ProductosViewSet, CombosViewSet, ProductoParaRecetaViewSet, ProductoViewSet, RecetaViewSet, RecetasRecientesViewSet, SumateViewSet, SuscripcionesViewSet, CarouselViewSet
+from api.views import BazarPremiumViewSet, CombosViewSet, ProductosViewSet, CombosViewSet, ProductoParaRecetaViewSet, ProductoViewSet, RecetaViewSet, RecetasRecientesViewSet, SumateViewSet, SuscripcionesViewSet, CarouselViewSet, ComplementosViewSet
 
 router = routers.DefaultRouter()
 router.register(r'producto', ProductoViewSet)
 router.register(r'receta', RecetaViewSet)
 router.register(r'recetas_recientes', RecetasRecientesViewSet)
 router.register(r'productos', ProductosViewSet)
-router.register(r'lineamarsala', CombosViewSet)
+router.register(r'combos', CombosViewSet)
 router.register(r'bazarpremium', BazarPremiumViewSet)
 router.register(r'productospararecetas', ProductoParaRecetaViewSet)
 router.register(r'suscripciones', SuscripcionesViewSet)
 router.register(r'sumate', SumateViewSet)
 router.register(r'carousel', CarouselViewSet)
+router.register(r'complementos', ComplementosViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
